b6064d840b0a2132ac1ec66472e918270731122c
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2021-12-30 14:29:14 +0200

    Fix eslint linter errors

--- index.js
+++ index.js
@@ -3,65 +3,62 @@ let bookCounter = 0;
 const inputForm = document.querySelector('#addBooks');
 const displayList = document.querySelector('#bookList');
 
-const showBooksList = (arr) => {
-	console.log(bookCollection)
-	if (arr.length <= 0) {
-		document.getElementById('bookList').innerHTML = '<h2> List of Books </h2> <ul><li> EMPTY LIST OF BOOKS <li></ul>';
-	} else {
-		const listBook = arr.map((book) => `
-			<li> '${book.title}' by ${book.author}
-			<button type='button' id='${book.id}' class='remove-button'>Remove</button>
-			</li>`).join('');
-		displayList.innerHTML = `<h2> List of Books </h2> <ul> ${listBook} <ul>`;
-	}
-	displayList.addEventListener('click', (e) => {
-		if (e.target.classList.contains('remove-button')) {
-			removeBooks(e);
-		}
-	});
-}
-
 const addBooks = (titleValue, authorValue) => {
-	bookCounter += 1;
-	bookCollection = [...bookCollection,{id: bookCounter ,title:titleValue, author:authorValue}];
-	localStorage.setItem('awesomeBooksCollection', JSON.stringify(bookCollection));
-}
-
-const removeBooks = (ev) => {
-	const buttonId = ev.target.id;
-	bookCollection = bookCollection.filter(
-		(y) => y !== bookCollection[bookCollection.findIndex(
-			(x) => x.id === parseInt(buttonId, 10),
-		)],
-	);
-	localStorage.setItem('awesomeBooksCollection', JSON.stringify(bookCollection));
-	showBooksList(bookCollection);
-}
-
-const init = () => {
-	const dataGet = localStorage.getItem('awesomeBooksCollection');
-	console.log(dataGet)
-	const data = JSON.parse(dataGet);
-	if (data) {
-		bookCollection = data;
-	}
-	showBooksList(bookCollection);
-	displayList.addEventListener('click', (e) => {
-		if (e.target.classList.contains('remove-button')) {
-			removeBooks(e);
-		}
-	});
+  bookCounter += 1;
+  bookCollection = [...bookCollection, { id: bookCounter, title: titleValue, author: authorValue }];
+  localStorage.setItem('awesomeBooksCollection', JSON.stringify(bookCollection));
 };
 
+const showBooksList = (arr) => {
+  if (arr.length <= 0) {
+    document.getElementById('bookList').innerHTML = '<h2> List of Books </h2> <ul><li> EMPTY LIST OF BOOKS <li></ul>';
+  } else {
+    const listBook = arr.map((book) => `
+      <li> '${book.title}' by ${book.author}
+      <button type='button' id='${book.id}' class='remove-button'>Remove</button>
+      </li>`).join('');
+    displayList.innerHTML = `<h2> List of Books </h2> <ul> ${listBook} <ul>`;
+  }
+  displayList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove-button')) {
+      // eslint-disable-next-line no-use-before-define
+      removeBooks(e);
+    }
+  });
+};
+
+const removeBooks = (ev) => {
+  const buttonId = ev.target.id;
+  bookCollection = bookCollection.filter(
+    (y) => y !== bookCollection[bookCollection.findIndex(
+      (x) => x.id === parseInt(buttonId, 10),
+    )],
+  );
+  localStorage.setItem('awesomeBooksCollection', JSON.stringify(bookCollection));
+  showBooksList(bookCollection);
+};
+
+const init = () => {
+  const dataGet = localStorage.getItem('awesomeBooksCollection');
+  const data = JSON.parse(dataGet);
+  if (data) {
+    bookCollection = data;
+  }
+  showBooksList(bookCollection);
+  displayList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove-button')) {
+      removeBooks(e);
+    }
+  });
+};
 
 // Event Listeners
 window.addEventListener('DOMContentLoaded', init);
-inputForm.addEventListener('submit', event => {
+inputForm.addEventListener('submit', (event) => {
   event.preventDefault();
-	const titleValue = document.getElementById("booktitle").value;
-	const authorValue = document.getElementById("authorname").value;
-	addBooks(titleValue, authorValue);
-	inputForm.reset();	
-	showBooksList(bookCollection);
+  const titleValue = document.getElementById('booktitle').value;
+  const authorValue = document.getElementById('authorname').value;
+  addBooks(titleValue, authorValue);
+  inputForm.reset();
+  showBooksList(bookCollection);
 });
-
